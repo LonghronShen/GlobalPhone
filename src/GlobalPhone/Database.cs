0067f7634916aee654a994ddf9849b759883bef6
Steven Hangger <longhronshen@hotmail.com>
2018-08-27 11:25:48 +0800

    fix

--- src/GlobalPhone/Database.cs
+++ src/GlobalPhone/Database.cs
@@ -39,9 +39,9 @@ namespace GlobalPhone
             return new Database(serializer.Deserialize<object[]>(stream));
         }
 
-        public static Context CreateContext(Database db = null, IDeserializer serializer = null)
+        public Context CreateContext(IDeserializer serializer = null)
         {
-            return new Context(serializer, db);
+            return new Context(serializer, this);
         }
 
         public bool TryGetRegion(int countryCode, out Region value)
